ad7494bc647a04516f78fed7bacb19881e15c31d
kangjw <71870130+kangjwme@users.noreply.github.com>
2021-03-08 10:57:08 +0800

    Create spam.py

---
+++ spam.py
@@ -0,0 +1,38 @@
+import smtplib
+import random
+import time
+from email.mime.text import MIMEText
+from email.header import Header
+
+#基本資訊調查
+smtpaddress = input("SMTP伺服器(預設port為587)：")
+sender = input("寄件人電子信箱：")
+senderpasswd = input("寄件人電子信箱密碼：")
+receiver = input("收件人電子信箱：")
+#SMTP驗證，初始化
+smtp=smtplib.SMTP(smtpaddress, 587)
+smtp.ehlo()
+smtp.starttls()
+smtp.login(sender,senderpasswd)
+
+#郵件寄件人、收件人設定
+from_addr=sender
+to_addr=receiver
+
+#內文設定(預設100封)
+for i in range(100):
+    content = "你知道這是我傳的第"+str(i+1)+"封嗎？"
+    msg = MIMEText(content, 'plain', 'utf-8')
+    msg['From'] = Header(sender, 'utf-8')
+    msg['To'] =  Header(receiver, 'utf-8')
+    subject = "你知道這是我傳的第"+str(i+1)+"封嗎？" 
+    msg['Subject'] = Header(subject, 'utf-8')
+
+    status=smtp.sendmail(from_addr, to_addr, msg.as_string())#加密文件，避免私密信息被截取
+
+    if status=={}:
+        print("郵件傳送成功!")
+    else:
+        print("郵件傳送失敗!")
+    time.sleep(10)
+smtp.quit()
